205e77ef0064aa694a84a2ba7c824de8315069b6
Jasmine Huang <tinjasminee@gmail.com>
2020-05-25 11:48:11 +0800

    finish

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,20 @@
 class QuestionsController < ApplicationController
   def ask
-
   end
+
+  def answer
+
+    @question = params[:question]
+
+    if @question == "I am going to work"
+      @output = "Great!"
+    elsif @question.end_with?("?")
+      @output = "Silly question, get dressed and go to work!"
+    else
+      @output = "I don't care, get dressed and go to work!"
+    end
+     return @output
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/ask', to: "questions#ask", as: :ask
+  get '/answer', to: "questions#answer", as: :answer
 end
